9bfdd344b876cbfd9c04f9efa322420104391432
avizcarra <a20181454@pucp.edu.pe>
2022-06-07 19:50:47 -0500

    Correcion de entidades

---
+++ src/main/java/com/example/lab6/Repository/DistribuidoraRepository.java
@@ -0,0 +1,7 @@
+package com.example.lab6.Repository;
+
+import Entity.Distribuidora;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DistribuidoraRepository extends JpaRepository<Distribuidora, Integer> {
+}
