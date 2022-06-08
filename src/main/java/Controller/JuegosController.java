bc05b70aece04d9fa5fcc745b0164fecb469aa14
Ronny Pastor <ronnypastorkolmakov@gmail.com>
2022-06-07 19:50:41 -0500

    Juego controller

--- src/main/java/Controller/JuegosController.java
+++ src/main/java/Controller/JuegosController.java
@@ -1,9 +1,41 @@
 package Controller;
 
+import Entity.Juego;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
 public class JuegosController {
+
+    @Autowired
+
+
+    @GetMapping("/juego")
+    public List<Juego> listarJuegos(){
+        return ;
+    }
+
+    @PostMapping("/juego")
+    public ResponseEntity<HashMap<String, Object>> guardarJuego(@RequestBody Juego juego){
+
+    }
+
+
+    @PutMapping("/juego")
+    public ResponseEntity<HashMap<String, Object>> actualizarJuego(@RequestBody Juego juego){
+
+    }
+
+    @DeleteMapping("/juego/{id}")
+    public ResponseEntity<HashMap<String, Object>> borrarJuego(@PathVariable("id") String idStr){
+
+
+    }
 
 
 
